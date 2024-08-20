a297e7ee6a601efe0d4309be75d13f722c22d222
Kashaf Shaikh <kashaf@example.com>
2024-08-20 21:43:18 +0500

    push my day23 to github

---
+++ day23.js
@@ -0,0 +1,29 @@
+// Day 23
+// ### Question 1: Calculate the Factorial of a Number
+// Task: Write a function calculateFactorial that takes a number as input and returns its factorial.
+// Hint: Factorial means multiplying a number by all positive integers less than it. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+function calculateFactorial(num) {
+    var res = 1;
+    for (var i = num; i >= 1; i--) {
+        res *= i;
+    }
+    return res;
+}
+console.log(calculateFactorial(5)); //output 5 * 4 * 3 * 2 * 1 = 120.
+console.log(calculateFactorial(6)); //output 6 * 5 * 4 * 3 * 2 * 1 = 720 
+// ### Question 2: Convert a Number to Binary (Without Using Built-in Functions)
+// Task: Write a function convertToBinary that takes a number as input and converts it into its binary form.
+// Hint: Repeatedly divide the number by 2 and note down the remainder. Join the remainders in reverse order to get the binary representation.
+function convertToBinary(num) {
+    if (num === 0)
+        return "0";
+    var binary = "";
+    while (num > 0) {
+        var reminder = num % 2;
+        binary = reminder + binary;
+        num = Math.floor(num / 2);
+    }
+    return binary;
+}
+console.log(convertToBinary(13)); // Output: "1101"
+console.log(convertToBinary(10)); // Ouput : "1010"
